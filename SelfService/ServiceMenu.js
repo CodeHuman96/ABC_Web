3565660761c49feb108d949d0213964c981e5aa1
shivasai7392 <shivasai7392@gmail.com>
2018-08-11 23:19:03 +0530

    ss

---
+++ SelfService/ServiceMenu.js
@@ -0,0 +1,20 @@
+function profile()
+{
+	window.location.href="Myprofile.html"
+}
+function chan()
+{
+	window.location.href="ChangePassword.html"
+}
+function acc()
+{
+	window.location.href="Accounts.html"
+}
+function mkser()
+{
+	window.location.href="ServiceRequest.html"
+}
+function viewser()
+{
+	window.location.href="ViewRequestStatus.html"
+}
