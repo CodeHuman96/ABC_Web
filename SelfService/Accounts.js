cbd229e2c6d83e3b69dc1034ac049177444755ac
shivasai7392 <shivasai7392@gmail.com>
2018-08-12 01:30:32 +0530

    ss

--- SelfService/Accounts.js
+++ SelfService/Accounts.js
@@ -6,3 +6,9 @@ function back1()
 {
 	window.location.href="Accounts.html"
 }
+function selectrow()
+{
+	var Row = document.getElementById("somerow");
+var Cells = Row.getElementsByTagName("td");
+window.location.href="Statement.html"
+}
